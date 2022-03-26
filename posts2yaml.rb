38c79227e560d8749ed1596c077033532eb49dab
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-03-26 17:01:17 +0000

    posts2yaml.rb - handle missing and duplicated slugs/titles
    Construct our own slugs, and detect duplication.  Include the ID for
    final disambiguation.

--- posts2yaml.rb
+++ posts2yaml.rb
@@ -15,11 +15,29 @@ dataUrl = "https://api.archive.rushkoff.com/posts?_limit=-1"
 
 def outputTsv(path, data)
   headers = data[0].keys
-
+  slugs = {}
+  
   data.each do |a|
-    name = "#{path}/#{a['slug']}.md"
-    warn "writing #{name}"
+    title = a['title']
+    slug = a['title']
+             .strip
+             .downcase
+             .gsub(/[^\s\w]+/, '')
+             .gsub(/\s+/, '-')[0,40]
+    id = "%04d" % a['id']
+    name = "#{path}/#{id}-#{slug}.md"
     body = a.delete 'body'
+    if slugs[title] then
+      slugs[title] += 1
+      warn "Potential name clash: title #{title} appears #{slugs[title]} times"
+    end
+
+    slugs[title] = 1
+    if File.exist? name then
+      warn "Overwriting #{name}"
+    else
+      warn "Writing #{name}"
+    end
     File.open(name, 'w') do |str|
       str.puts a.to_yaml
       str.puts "---\n", body
