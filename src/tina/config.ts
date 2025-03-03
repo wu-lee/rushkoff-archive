730f8a54e9510bec476384d847a9ae5626262e84
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-03-03 11:49:17 +0000

    src/tina/config.ts - adjust publicFolder / mediaRoot
    This configuration seems to be correct - "assets" seem to mean the
    images uploaded along with content, not the theme's assets folder.

--- src/tina/config.ts
+++ src/tina/config.ts
@@ -28,8 +28,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "assets",
+      mediaRoot: "uploads",
+      publicFolder: "",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
